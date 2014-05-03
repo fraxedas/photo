ed6d11bf299a30ff188c9bdd53377dc591401e3c
fraxedas <fraxedas@gmail.com>
2014-05-03 19:33:28 -0400

    Build nuget package from the project properties and using the command line

--- src/build.bat
+++ src/build.bat
@@ -1,14 +1,12 @@
 Echo "Upgrade to the latest version of NuGet.exe"
 .nuget\NuGet Update -self
 
-Echo "Store the API Key"
-.nuget\NuGet SetApiKey b6fd4d0a-c0c3-4b83-9723-5074b975dc53
-
 Echo "Create and buils the NuGet Package"
 cd photo.exif
-#..\.nuget\NuGet spec 
-..\.nuget\NuGet Pack photo.exif.csproj
+..\.nuget\NuGet Pack photo.exif.csproj -Prop Platform=AnyCPU
 cd ..
 
 Echo "Publish your package"
-#.nuget\NuGet Push YourPackage.nupkg
+cd photo.exif
+..\.nuget\NuGet Push photo.exif.*.nupkg
+cd ..
